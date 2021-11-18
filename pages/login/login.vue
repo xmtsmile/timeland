d09ec2cfac9d717a5406c44ed038f5752e7be3e4
xumt <xumtQasto-inc.com>
2021-11-18 16:31:04 +0800

    revert sth

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -18,22 +18,18 @@
 					global.userInfo = data.userInfo
 					wxLogin().then(code => {
 						console.log('code----', code)
-						//请求后端登录接口
-						// http.post('login', {code: code}).then(res => {
-						// 	if (res.token) {
-						// 		uni.setStorage({
-						// 		    key: 'token',
-						// 		    data: res.token,
-						// 		    success: function () {
-						// 		        uni.switchTab({
-						// 		        	url:'../tab1/index'
-						// 		        })
-						// 		    }
-						// 		});
-						// 	}
-						// })
-						uni.switchTab({
-							url:'../tab1/index'
+						http.post('login', {code: code}).then(res => {
+							if (res.token) {
+								uni.setStorage({
+								    key: 'token',
+								    data: res.token,
+								    success: function () {
+								        uni.switchTab({
+								        	url:'../tab1/index'
+								        })
+								    }
+								});
+							}
 						})
 					})
 				})
